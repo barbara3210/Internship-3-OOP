fdc57c6a12002176e2a323c0308ffb18ee61744c
barbara3210 <bivkovic@pmfst.hr>
2023-11-23 09:54:44 +0100

    Add project files.

---
+++ Phone-book-app/Classes/Call.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone_book_app.Classes
+{
+    public class Call
+    {
+        public DateTime CallTime { get; set; }
+        public string CallStatus { get; set; }
+    }
+}
---
+++ Phone-book-app/Classes/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone_book_app.Classes
+{
+    public class Contact
+    {
+
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Preference { get; set; }
+    }
+}
