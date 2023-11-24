7db9b40bee83fd846afaa9c3c6b2feee836807e0
barbara3210 <bivkovic@pmfst.hr>
2023-11-25 00:41:52 +0100

    add_print_manage_contacts

--- Phone-book-app/Classes/Contact.cs
+++ Phone-book-app/Classes/Contact.cs
@@ -6,17 +6,28 @@ using System.Threading.Tasks;
 
 namespace Phone_book_app.Classes
 {
+    public enum ContactPreference
+    {
+        Favorite,
+        Normal,
+        Blocked
+    }
     public class Contact
     {
 
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
-        public string Preference { get; set; }
+        public ContactPreference Preference { get; set; }
         
         public Contact(string name, string phone)
         {
             Name = name;
             PhoneNumber = phone;
+            Preference = ContactPreference.Normal;
+        }
+        public Contact(ContactPreference preference)
+        {
+            Preference = preference;
         }
     }
 }
--- Phone-book-app/Program.cs
+++ Phone-book-app/Program.cs
@@ -1,12 +1,12 @@
 ﻿using Phone_book_app.Classes;
 
-var calls = new Dictionary<Contact, List<Call>>();
+var contacts_dict = new Dictionary<Contact, List<Call>>();
 
 
-while(true)
+while (true)
 {
 
-    Console.WriteLine("~MENU~\n"
+    Console.WriteLine("~ MENU ~\n"
     + "1 - Ispis svih kontakata\n"
     + "2 - Dodavanje novih kontakata u imenik\n"
     + "3 - Brisanje kontakata iz imenika\n"
@@ -18,15 +18,20 @@ while(true)
     Console.WriteLine("Vas odabir: ");
     var choice=Console.ReadLine();
 
-    if(int.TryParse(choice, out int your_choice))
+    if(!int.TryParse(choice, out int your_choice))
+    {
+        Console.WriteLine("Ponovi unos");
+        return;        
+    }
+    else
     {
         switch (your_choice)
         {
             case 1:
-                PrintAllContact();
+                PrintAllContact(contacts_dict);
                 break;
             case 2:
-                AddNewContact();
+                AddNewContact(contacts_dict);
                 break;
             case 3:
                 DeleteContact();
@@ -35,7 +40,7 @@ while(true)
                 EditPreferenceContact();
                 break;
             case 5:
-                ManageContact();
+                ManageContact(contacts_dict);
                 break;
             case 6:
                 PrintAllCalls();
@@ -45,27 +50,42 @@ while(true)
             default: break;
         }
     }
-    else { Console.WriteLine("Ponovi unos"); }
-
-
-    Console.WriteLine("~SUBMENU~\n"
-    + "1 - Ispis svih poziva\n"
-    + "2 - Novi poziv\n"
-    + "3 - Izlaz iz podmenua\n"
-
-    );
-}
-
-
-
-
-static void PrintAllContact()
-{
 
 }
-static void AddNewContact()
-{
 
+
+static void PrintAllContact(Dictionary<Contact, List<Call>> contactsDict)
+{
+    Console.WriteLine("~ SVI KONTAKTI ~");
+    foreach (var contact in contactsDict.Keys)
+    {
+        Console.WriteLine($"{contact.Name} - {contact.PhoneNumber} ({contact.Preference}\n)");
+    }
+}
+static void AddNewContact(Dictionary<Contact, List<Call>> contactsDict)
+{
+    Console.WriteLine("Unesite ime novog kontakta:");
+    var contactName = Console.ReadLine();
+    if (string.IsNullOrEmpty(contactName))
+    {
+        Console.WriteLine("Neispravan unos imena kontakta.");
+    }
+    else
+    {
+        Console.WriteLine("Unesite broj:");
+        var contactNum=Console.ReadLine();
+        if (string.IsNullOrEmpty(contactName))
+        {
+            Console.WriteLine("Neispravan unos imena kontakta.");
+        }
+        else
+        {
+            var newContact = new Contact(contactName, contactNum);
+            contactsDict.Add(newContact, new List<Call>());
+            Console.WriteLine("Kontakt dodan u imenik.");
+        }
+    }
+    
 }
 static void DeleteContact()
 {
@@ -75,8 +95,61 @@ static void EditPreferenceContact()
 {
 
 }
-static void ManageContact()
+static void ManageContact(Dictionary<Contact, List<Call>> contactsDict)
 {
+    Console.WriteLine("Unesite ime kontakta: ");
+    var contact_name= Console.ReadLine();
+
+    if(contact_name == null) { Console.WriteLine("Ponovite unos"); }
+    else
+    {
+        
+        var contactToManage = contactsDict.Keys.FirstOrDefault(c => c.Name == contact_name);
+        if (contactToManage != null)
+        {
+            while (true)
+            {
+                Console.WriteLine("~ SUBMENU ~\n"
+               + "1 - Ispis svih poziva\n"
+               + "2 - Novi poziv\n"
+               + "3 - Izlaz iz podmenua\n"
+                 );
+
+                Console.WriteLine("Unesite svoj odabri:");
+                var choice = Console.ReadLine();
+
+                if (!int.TryParse(choice, out int sub_choice))
+                {
+                    Console.WriteLine("Ponovite unos");
+                    return;
+                }
+                else
+                {
+                    switch (sub_choice)
+                    {
+                        case 1:
+                            //PrintAllCalls();
+                            break;
+                        case 2:
+
+                            break;
+                        case 3:
+                            break;
+                        default: break;
+                    }
+                }
+
+
+            }
+        }
+        else
+        {
+            Console.WriteLine("Kontakt ne postoji u imeniku");
+        }
+
+        
+    }
+    
 
 }
 static void PrintAllCalls()
